0fb6f1dc1b586dc07ee746cb9ad35ae46ad81a7b
interkosmos <pengel@hs-nb.de>
2017-05-05 21:24:07 +0200

    Updated make file.

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Creating executables ...
-"C:\Program Files\Python\Python35-32\Scripts\pyinstaller-script.py" --clean --noconfirm --icon=dabamos.ico --hidden-import "module.database" --hidden-import "module.export" --hidden-import "module.gpio" --hidden-import "module.notification" --hidden-import "module.port" --hidden-import "module.processing" --hidden-import "module.prototype" --hidden-import "module.schedule" --hidden-import "module.totalstation" --hidden-import "module.virtual" openadms.py
+"C:\Program Files\Python\Python35-32\Scripts\pyinstaller-script.py" --clean --noconfirm --icon=dabamos.ico --hidden-import "module.database" --hidden-import "module.export" --hidden-import "module.gpio" --hidden-import "module.notification" --hidden-import "module.port" --hidden-import "module.processing" --hidden-import "module.prototype" --hidden-import "module.schedule" --hidden-import "module.server" --hidden-import "module.totalstation" --hidden-import "module.virtual" openadms.py
 
 echo Done.
 pause
