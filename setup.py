412972e1c292383b8b233409621bdf45911c8ab5
interkosmos <pengel@hs-nb.de>
2017-09-30 23:44:44 +0200

    Refactoring.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python3.6
-"""
-Copyright (c) 2017 Hochschule Neubrandenburg an other contributors.
-
-Licenced under the EUPL, Version 1.1 or - as soon they will be approved
-by the European Commission - subsequent versions of the EUPL (the
-"Licence");
-
-You may not use this work except in compliance with the Licence.
-
-You may obtain a copy of the Licence at:
-
-    https://joinup.ec.europa.eu/community/eupl/og_page/eupl
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the Licence is distributed on an "AS IS" basis,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the Licence for the specific language governing permissions and
-limitations under the Licence.
-"""
 
 """Setup for cx_Freeze
 
@@ -26,8 +7,8 @@ Just run
 
     $ python setup.py build
 
-or start the batch file `win_make.bat`. All files are stored in the sub-folder
-`dist`. For more information, please see https://www.dabamos.de/.
+or start the batch file `win_make_cx_freeze.bat`. All files are stored in the
+sub-folder `dist`. For more information, please see https://www.dabamos.de/.
 """
 
 __author__ = 'Philipp Engel'
@@ -35,7 +16,10 @@ __copyright__ = 'Copyright (c) 2017 Hochschule Neubrandenburg'
 __license__ = 'EUPL'
 
 import sys
+
 from cx_Freeze import setup, Executable
+
+from core import version
 
 
 build_exe_options = {
@@ -45,7 +29,6 @@ build_exe_options = {
                  'packaging',
                  'module.database',
                  'module.export',
-                 'module.linux',
                  'module.notification',
                  'module.port',
                  'module.processing',
@@ -75,13 +58,14 @@ if sys.platform == 'win32':
 
 executables = [
     Executable('openadms.py', base=base),
-    Executable('openadms-gui.pyw', base=base)
+    Executable('openadms-launcher.pyw', base=base)
 ]
 
 setup(name='OpenADMS',
-      version='0.6',
+      version=OPENADMS_VERSION,
       description='OpenADMS',
       options={
           'build_exe': build_exe_options
       },
       executables=executables)
+
