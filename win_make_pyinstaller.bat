4e34d3532e2bef23721bd8d397b12cf4835c1688
interkosmos <pengel@hs-nb.de>
2017-10-05 10:52:36 +0200

    Added console app.

--- win_make_pyinstaller.bat
+++ win_make_pyinstaller.bat
@@ -3,6 +3,7 @@
 echo Creating executables with Python 3.6 ...
 
 pyinstaller --clean --windowed --noconfirm --icon="res\img\dabamos.ico" --hidden-import "gooey" --hidden-import "openadms" --hidden-import "module.database" --hidden-import "module.export" --hidden-import "module.notification" --hidden-import "module.port" --hidden-import "module.processing" --hidden-import "module.prototype" --hidden-import "module.schedule" --hidden-import "module.server" --hidden-import "module.testing" --hidden-import "module.totalstation" --hidden-import "module.virtual" openadms-launcher.pyw
+pyinstaller --clean --noconfirm --icon="res\img\dabamos.ico" --hidden-import "module.database" --hidden-import "module.export" --hidden-import "module.notification" --hidden-import "module.port" --hidden-import "module.processing" --hidden-import "module.prototype" --hidden-import "module.schedule" --hidden-import "module.server" --hidden-import "module.testing" --hidden-import "module.totalstation" --hidden-import "module.virtual" openadms.py
 
 mkdir dist\openadms-launcher\data
 
